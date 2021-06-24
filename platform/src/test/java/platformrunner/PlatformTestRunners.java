9564339908c874c2f1fdd19b9858ea7a20076312
csonparote <chetan.sonparote@scispl.com>
2021-06-24 17:56:33 +0530

    cucumber testng
    runner executed from testng.xml

--- platform/src/test/java/platformrunner/PlatformTestRunners.java
+++ platform/src/test/java/platformrunner/PlatformTestRunners.java
@@ -6,6 +6,7 @@ import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
 import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @RunWith(ExtendedCucumber.class)
 @ExtendedCucumberOptions(jsonReport = "target/cucumber.json", overviewReport = true, outputFolder = "target")
@@ -16,5 +17,5 @@ import cucumber.api.CucumberOptions;
 }, glue = { "platformstepdefinition" }, tags = { "@FunctionalTest" }, plugin = { "pretty",
 		"html:target/cucumber_html_report", "json:target/cucumber.json", "pretty:target/cucumber-pretty.txt",
 		"usage:target/cucumber-usage.json", "junit:target/cucumber_html_report/junit_platform.xml" })
-public class PlatformTestRunners {
+public class PlatformTestRunners extends AbstractTestNGCucumberTests {
 }
