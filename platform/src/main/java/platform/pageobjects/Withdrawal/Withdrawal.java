9530ca3c98f399ee5872875de29a09a689dbf1a7
Sahaj Balgunde <56796480+sahajisy@users.noreply.github.com>
2021-07-28 13:51:07 +0530

    Withdrawal Test Script Files

---
+++ platform/src/main/java/platform/pageobjects/Withdrawal/Withdrawal.java
@@ -0,0 +1,67 @@
+package platform.pageobjects.Withdrawal;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import platform.pageobjects.Authentication.LoginPage;
+import utils.CommonFunctions;
+
+/**
+ * @Author : Sahaj Balgunde
+ * @Date of Creation : 12 Jul 2021
+ * @Description: Additional Locator variables and Methods for withdrawal page.
+ */
+
+public class Withdrawal {
+	List<String[]> testdata;
+	public WebDriver driver;
+	private static Logger log = LogManager.getLogger(LoginPage.class.getName());
+
+	public Withdrawal(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath = "//span[@class='checkmark']")
+	public WebElement withdrawalCheck;
+
+	@FindBy(xpath = "//a[@class='button button--gray button--medium button--coupon-back']//span[contains(text(),'戻る')]")
+	public WebElement back;
+
+	@FindBy(xpath = "//button[@role='button']")
+	public WebElement withdrawAccount;
+
+	@FindBy(xpath = "//h1[contains(@class,'')]//a[contains(@class,'logo')]")
+	public WebElement topButton;
+
+	public void accountWithdrawal() throws Exception {
+		CommonFunctions.scrolltoElement(withdrawalCheck);
+		if (CommonFunctions.isElementClickable(withdrawalCheck)) {
+			withdrawalCheck.click();
+			System.out.println("Withdrawal Checked");
+
+			if (CommonFunctions.isElementClickable(withdrawAccount)) {
+				withdrawAccount.click();
+				System.out.println("Account Deleted");
+			}
+		}
+	}
+
+	public void withdrawButtons() throws Exception {
+
+		CommonFunctions.waitForVisiblity(back, 3);
+		back.click();
+		log.info("Check the back Button");
+
+		CommonFunctions.waitForVisiblity(topButton, 3);
+		topButton.click();
+		log.info("Check the Top Button");
+
+	}
+}
---
+++ platform/src/test/java/platformfeatures/PF_Withdrawal.feature
@@ -0,0 +1,58 @@
+
+#Author: Sahaj Balgunde
+#Keywords Summary : Withdawal Test with Checkbox and Without Checkbox
+
+
+
+Feature: Withdraw user account
+@FunctionalTest 
+Scenario Outline: User is withdrawing the account
+
+#Given User is on Withdrawal Page
+#When User clicks on accept checkbox
+#Then the check box is clickable
+
+
+#Scenario: User is withdrawing the account
+
+Given User is on Withdrawal Page ticks the checkbox and withdraw account <browser>
+Then Account gets deleted
+
+
+Examples:
+|browser|
+|chrome	|
+
+#Scenario: User is withdrawing the account
+
+#Given User is on Withdrawal Page
+#When User clicks on "é€€ä¼šã�™ã‚‹" Button without clicking checkbox. 
+#Then Account doesn't get deleted
+
+
+#Scenario: User is withdrawing the account.
+
+#Given User is on Withdrawal Page.
+#When User clicks on "æˆ»ã‚‹" Button. 
+#Then Button Redirects to top page.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
