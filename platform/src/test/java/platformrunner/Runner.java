02333a3f0c5657e961b37ba585e92f56c411ba2c
csonparote <chetan.sonparote@scispl.com>
2021-07-28 19:31:29 +0530

    Update Runner.java
    only single tag used

--- platform/src/test/java/platformrunner/Runner.java
+++ platform/src/test/java/platformrunner/Runner.java
@@ -29,8 +29,7 @@ import utils.ObjectHelper;
 
 		glue = { "platformstepdefinition" },
 
-		tags = { "@PF_test", "~@FeatureTest", "~@CreateAccountTest", "~@Smoke", "~@PFInvalidCC",
-				"~@PFAccountAndSecurity" },
+		tags = { "@PF_test" },
 
 		plugin = { "pretty", "html:target/cucumber_html_report", "json:target/cucumber.json",
 				"pretty:target/cucumber-pretty.txt", "usage:target/cucumber-usage.json",
