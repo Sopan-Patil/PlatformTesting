c43aa60b422e24f359fe23b99aae1e5456a56a1c
csonparote <chetan.sonparote@scispl.com>
2021-08-03 08:58:49 +0530

    Update Runner.java
    added null for optional

--- platform/src/test/java/platformrunner/Runner.java
+++ platform/src/test/java/platformrunner/Runner.java
@@ -44,10 +44,10 @@ public class Runner extends AbstractTestNGCucumberTests {
 
 	@BeforeTest
 	@org.testng.annotations.Parameters(value = { "mode", "browser", "config", "environment" })
-	public void setUpBrowser(@org.testng.annotations.Optional("local") String mode,
-			@org.testng.annotations.Optional("chrome") String browser,
-			@org.testng.annotations.Optional("local.conf.json") String config,
-			@org.testng.annotations.Optional("chrome") String environment) throws Exception {
+	public void setUpBrowser(@org.testng.annotations.Optional("null") String mode,
+			@org.testng.annotations.Optional("null") String browser,
+			@org.testng.annotations.Optional("null") String config,
+			@org.testng.annotations.Optional("null") String environment) throws Exception {
 		// public void setUpBrowser(String mode, String browser, String config, String
 		// environment) throws Exception {
 
