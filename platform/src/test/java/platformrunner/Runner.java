ff696331e815f46ad669928d82319792ee440885
csonparote <chetan.sonparote@scispl.com>
2021-08-03 09:07:09 +0530

    Update Runner.java

--- platform/src/test/java/platformrunner/Runner.java
+++ platform/src/test/java/platformrunner/Runner.java
@@ -44,12 +44,11 @@ public class Runner extends AbstractTestNGCucumberTests {
 
 	@BeforeTest
 	@org.testng.annotations.Parameters(value = { "mode", "browser", "config", "environment" })
-	public void setUpBrowser(@org.testng.annotations.Optional("null") String mode,
-			@org.testng.annotations.Optional("null") String browser,
-			@org.testng.annotations.Optional("null") String config,
-			@org.testng.annotations.Optional("null") String environment) throws Exception {
-		// public void setUpBrowser(String mode, String browser, String config, String
-		// environment) throws Exception {
+//	public void setUpBrowser(@org.testng.annotations.Optional("null") String mode,
+//			@org.testng.annotations.Optional("null") String browser,
+//			@org.testng.annotations.Optional("null") String config,
+//			@org.testng.annotations.Optional("null") String environment) throws Exception {
+	public void setUpBrowser(String mode, String browser, String config, String environment) throws Exception {
 
 		newBaseClass = new NewBaseClass();
 
