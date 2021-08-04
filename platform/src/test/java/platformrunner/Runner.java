ad0eff4d18012efc6589ee268c8f8fbae3be4cb3
RahulSmartcloudPFQA <85993722+RahulSmartcloudPFQA@users.noreply.github.com>
2021-08-04 15:56:42 +0530

    @PF_CreateAccount update
    @PF_CreateAccount update

--- platform/src/test/java/platformrunner/Runner.java
+++ platform/src/test/java/platformrunner/Runner.java
@@ -29,7 +29,7 @@ import utils.ObjectHelper;
 
 		//tags = { "@PF_test" },
 
-	 tags = { "@Smoke" },
+	 tags = { "@PF_CreateAccount" },
 
 		plugin = { "pretty", "html:target/cucumber_html_report", "json:target/cucumber.json",
 				"pretty:target/cucumber-pretty.txt", "usage:target/cucumber-usage.json",
