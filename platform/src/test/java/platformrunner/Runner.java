e57deacee76e5984485afccf56baa894e7ac0571
RahulSmartcloudPFQA <85993722+RahulSmartcloudPFQA@users.noreply.github.com>
2021-08-10 10:04:18 +0530

    update

--- platform/src/test/java/platformrunner/Runner.java
+++ platform/src/test/java/platformrunner/Runner.java
@@ -27,9 +27,9 @@ import utils.ObjectHelper;
 
 		glue = { "platformstepdefinition" },
 
-		// tags = { "@PF_test" },
+		 tags = { "@PF_test" },
 
-		tags = { "@PFAccountAndSecurity, @PFAccountAndSecurityCancel" },
+		//tags = { "@PFAccountAndSecurity, @PFAccountAndSecurityCancel" },
 
 		plugin = { "pretty", "html:target/cucumber_html_report", "json:target/cucumber.json",
 				"pretty:target/cucumber-pretty.txt", "usage:target/cucumber-usage.json",
