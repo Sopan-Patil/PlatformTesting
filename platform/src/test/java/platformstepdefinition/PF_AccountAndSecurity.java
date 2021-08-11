d6d0d60903981e9d9a7441621fe93ceeb35eabb7
csonparote <chetan.sonparote@scispl.com>
2021-08-11 15:26:08 +0530

    Update PF_AccountAndSecurity.java

--- platform/src/test/java/platformstepdefinition/PF_AccountAndSecurity.java
+++ platform/src/test/java/platformstepdefinition/PF_AccountAndSecurity.java
@@ -62,7 +62,7 @@ public class PF_AccountAndSecurity extends NewBaseClass {
 	@And("^Validate Missing Values Of Account Information$")
 	public void Validate_Missing_Values_Of_Account_Information() throws Throwable {
 		AccountAndSecurity MissingValues = new AccountAndSecurity(driver);
-		MissingValues.CheckMissngValuesErrorOnAccountAndSecurityInformation();
+		MissingValues.checkMissngValuesErrorOnAccountAndSecurityInformation();
 	}
 
 }
