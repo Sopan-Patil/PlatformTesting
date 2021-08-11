b058731327e87fd69c26c54b84d8f9dc843ecce6
csonparote <chetan.sonparote@scispl.com>
2021-08-11 16:00:13 +0530

    PR changes

--- platform/src/main/java/base/NewBaseClass.java
+++ platform/src/main/java/base/NewBaseClass.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
