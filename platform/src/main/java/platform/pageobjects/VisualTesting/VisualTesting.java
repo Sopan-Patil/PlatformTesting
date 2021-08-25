bdd4c1bf7bd8cdd882b0e6b95971ef5a8d8af24a
csonparote <chetan.sonparote@scispl.com>
2021-08-25 09:06:10 +0530

    Update VisualTesting.java

--- platform/src/main/java/platform/pageobjects/VisualTesting/VisualTesting.java
+++ platform/src/main/java/platform/pageobjects/VisualTesting/VisualTesting.java
@@ -3,6 +3,7 @@ package platform.pageobjects.VisualTesting;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.percy.selenium.Percy;
@@ -12,13 +13,19 @@ public class VisualTesting {
 	private static Percy percy;
 
 	public static void main(String[] args) {
+		VisualTesting visualTesting = new VisualTesting();
+		visualTesting.testVisualData();
+	}
+
+	@Test
+	public void testVisualData() {
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
 		options.setHeadless(true);
 		driver = new ChromeDriver(options);
 		percy = new Percy(driver);
 
-		driver.get("https://example.com");
+		driver.get("https://a1.marugotoweb.jp/en/");
 		percy.snapshot("Java example");
 	}
 
