0258dc53e9209f17e5f38ed9db6ce54226e2f6b7
Sopan Patil <84448441+Sopan-Patil@users.noreply.github.com>
2021-08-27 17:34:40 +0530

    test

--- platform/src/main/java/platform/pageobjects/Authentication/LoginPage.java
+++ platform/src/main/java/platform/pageobjects/Authentication/LoginPage.java
@@ -168,7 +168,6 @@ public class LoginPage {
 			log.info("The user logout successfully");
 		}
 
-
 	}
 
 	public void navigateToHomePage() throws Exception {
@@ -280,47 +279,6 @@ public class LoginPage {
 	 * @Description: Method to login as prime member. to be parameterized with login
 	 *               later
 	 */
-
-	public void loginWithPrimeUser() throws Exception {
-
-		ExcelUtil excelUtil = new ExcelUtil();
-		excelUtil.setExcelFile("NewTestData.xlsx", "PrimeUser");
-		String email = excelUtil.getCellData("Email", 1);
-		String password = excelUtil.getCellData("Password", 1);
-
-		log.info("New User Email : " + email);
-		log.info("New User password : " + password);
-
-		if (CommonFunctions.waitForVisiblity(logInButton, waitTime)) {
-			logInButton.click();
-			log.info("Login button is clicked");
-
-		}
-
-		if (CommonFunctions.waitForVisiblity(emailtextfield, waitTime)) {
-			emailtextfield.click();
-			emailtextfield.sendKeys(email);
-
-		}
-
-		if (CommonFunctions.waitForVisiblity(passwordTextField, waitTime)) {
-
-			// passwordTextField.sendKeys("Test-123");
-			passwordTextField.click();
-
-			passwordTextField.sendKeys(password);
-			// System.out.println(passwordTextField.getText());
-		}
-
-		if (CommonFunctions.waitForVisiblity(SubmitButton, waitTime)) {
-			CommonFunctions.clickUsingJavaExecutor(SubmitButton);
-		}
-
-//		if (CommonFunctions.waitForVisiblity(SubmitButton, waitTime)) {
-//			CommonFunctions.clickUsingJavaExecutor(SubmitButton);
-//		}
-
-	}
 
 	public void loginWithPrimeUser() throws Exception {
 
